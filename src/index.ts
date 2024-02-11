ad58a7913db1164970f8c94ef58e9b8aaf6dd32d
dzehil02 <dzehil02@gmail.com>
2024-02-11 16:27:24 +0100

    feat: add webpack and typescript settings

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-    console.log('Server is running on port 4000');
+    console.log(`Server is running on port ${PORT}`);
 });
